e40e29006b86fd46382fe2a9511885f912d5f338
Wendy Nguyen <wendy.nguyen@gmail.com>
2021-08-30 13:50:37 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+//calender events array example:
+var calEvents = [
+  {
+    "time": "9am",
+    "description": "",
+    "status": ""
+  },
+  {
+    "time": "10am",
+    "description": "",
+    "status": ""
+  },
+  {
+    "time": "11am",
+    "description": "",
+    "status": ""
+  },
+  {
+    "time": "12pm",
+    "description": "",
+    "status": ""
+  },
+  {
+    "time": "1pm",
+    "description": "",
+    "status": ""
+  },
+  {
+    "time": "2pm",
+    "description": "",
+    "status": ""
+  },
+  {
+    "time": "3pm",
+    "description": "",
+    "status": ""
+  },
+  {
+    "time": "4pm",
+    "description": "",
+    "status": ""
+  },
+  {
+    "time": "5pm",
+    "description": "",
+    "status": ""
+  }
+]
+
+
+function loadCalendar () {
+// display the day in "Thursday, September 5th" format
+
+// create calendar grid
+
+// color code calendar grid so that past time blocks are grey, current is red and future is green
+
+// load events for the day
+};
+
+// add event listener for when time block clicked
+
+// add event listener for any save button
+
+
+// load calendar for the first time
+loadCalendar();
