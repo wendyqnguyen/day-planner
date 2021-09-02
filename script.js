6df5d165b5731d3466adf1ad12dbc7a3ca4b6c2e
Wendy Nguyen <wendy.nguyen@gmail.com>
2021-09-02 11:08:20 -0700

    check for current day and reset events saved in local storage accordingly

--- script.js
+++ script.js
@@ -7,73 +7,18 @@ var dayEl = document.querySelector("#currentDay");
 // current day
 var timestamp = moment();
 
-// var nineAm = document.querySelector("#nine-am-event");
-// console.log(nineAm);
-// nineAm.innerHTML= "some event";
-//calender events array example:
-var calEvents = [
-  {
-    "hour": "9",
-    "description": "event",
-    "status": ""
-  },
-  {
-    "hour": "10",
-    "description": "event",
-    "status": ""
-  },
-  {
-    "hour": "11",
-    "description": "event",
-    "status": ""
-  },
-  {
-    "hour": "12",
-    "description": "event",
-    "status": ""
-  },
-  {
-    "hour": "13",
-    "description": "event",
-    "status": ""
-  },
-  {
-    "hour": "14",
-    "description": "event",
-    "status": ""
-  },
-  {
-    "hour": "15",
-    "description": "event",
-    "status": ""
-  },
-  {
-    "hour": "16",
-    "description": "event",
-    "status": ""
-  },
-  {
-    "hour": "5pm",
-    "description": "event",
-    "status": ""
-  }
-]
 function colorCodeCalendar () {
-  // console.log("color code");
     // color code calendar, hours in the past are grey, current hour is red, future hours are gree
   for (var i=9; i< 18; i++){
     if(timestamp.hour() == i){
       var hourEl = document.querySelector(`#${CSS.escape(i)}`);
       hourEl.style.backgroundColor = "red";
-      console.log(i + " " + hourEl.getAttribute("style"));
     }else if (timestamp.hour()>i){
       var hourEl = document.querySelector(`#${CSS.escape(i)}`);
       hourEl.style.backgroundColor = "lightgrey";
-      console.log(i + " " + hourEl.getAttribute("background-color"));
     }else{
       var hourEl = document.querySelector(`#${CSS.escape(i)}`);
       hourEl.style.backgroundColor = "#77dd77";
-      console.log(i + " " + hourEl.getAttribute("background-color"));
     }
 }
 };
@@ -84,49 +29,57 @@ function loadCalendar () {
   var displayDay = timestamp.format('dddd') + " " + timestamp.format('MMMM') + " " + timestamp.format('Mo');
   dayEl.innerHTML = displayDay;
 
+
   // color code calendar grid so that past time blocks are grey, current is red and future is green
   colorCodeCalendar ()
+
   // load events for the day
   var eventEl = document.createElement("div");
-  // console.log(eventEl);
   for (var i = 9; i< 18; i++){
     eventEl = document.querySelector(`#${CSS.escape(i)}`);
     eventEl.innerText = localStorage.getItem(JSON.stringify(i))
-    // console.log(eventEl);
   }
 };
 
-
-
-
-var hourHandler = function (event) {
+var saveHandler = function (event) {
   // get target element from event
   var targetRowEl = event.target;
   targetRowEl = targetRowEl.closest(".saveBtn");
   targetRowEl = targetRowEl.previousElementSibling;
-  console.log(targetRowEl);
 
   // get text from hour row
   var eventDescription = targetRowEl.innerText;
-  console.log (eventDescription);
 
   // get which hour was clicked 
   var hour = targetRowEl.id;
-  console.log(hour);
-  localStorage.setItem(hour, eventDescription);
-  //get which hour cell was clicked
-  // var 
+  // save event to local storage
+  localStorage.setItem(hour, eventDescription); 
 
 }
-
-// add event listener for when time block clicked
-// pageContentEl.addEventListener("click", hourHandler);
 
 // add event listener for any save button
 var buttons = document.querySelectorAll(".save-btn").length;
 for (var i = 0; i < buttons ; i++) {
-    document.querySelectorAll(".save-btn")[i].addEventListener("click", hourHandler);
+    document.querySelectorAll(".save-btn")[i].addEventListener("click", saveHandler);
 }
 
+// compare current day to the current day value stored in local storage
+// if current day is not the same as the day saved in local storage, clear all events from local storage and save the current day value to local storage
+
+  if(!localStorage.getItem("today")){
+    localStorage.setItem("today", JSON.stringify(timestamp));
+  }else{
+    var day = JSON.parse(localStorage.getItem("today"));
+    var date1 = timestamp.format('YYYY-MM-DD');
+    var date2 = moment(day).format('YYYY-MM-DD');
+    localStorage.setItem("today", JSON.stringify(timestamp));
+    if (date2!=date1){
+      var eventEl = document.createElement("div");
+      for (var i = 9; i< 18; i++){
+        localStorage.setItem(JSON.stringify(i), "")
+      }
+    }
+  }
+  
 // load calendar for the first time
 loadCalendar();
